44cc344bd59d1ecb0e5005cb4196e8cf06b3fd06
StrangerTNY <2tonytu@gmail.com>
2024-09-13 08:49:17 +0200

    header sticky

--- components/simpleCenteredFooter.vue
+++ components/simpleCenteredFooter.vue
@@ -1,6 +1,7 @@
 <template>
-  <footer class="bg-secondary/30">
-    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+    <div class="border border-primary"></div>
+  <footer class="bg-secondary/25">
+    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-10 sm:py-15 lg:px-8">
       <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
         <div v-for="item in navigation.main" :key="item.name" class="pb-6">
           <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
