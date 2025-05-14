5a93b93ae0d0489e13166976bbd77d823ab1ce54
StrangerTNY <2tonytu@gmail.com>
2025-05-14 10:34:26 +0200

    insight tag plugin approach

---
+++ plugins/linkedin-insight.client.ts
@@ -0,0 +1,21 @@
+// plugins/linkedin-insight.client.ts
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin(() => {
+  if (process.client) {
+    // 1) Initialize the partner ID array
+    ;(window as any)._linkedin_partner_id = '7175042'
+    ;(window as any)._linkedin_data_partner_ids =
+      (window as any)._linkedin_data_partner_ids || []
+    ;(window as any)._linkedin_data_partner_ids.push(
+      (window as any)._linkedin_partner_id
+    )
+
+    // 2) Inject the insight.min.js script
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.async = true
+    script.src = 'https://snap.licdn.com/li.lms-analytics/insight.min.js'
+    document.head.appendChild(script)
+  }
+})
